5c38a99dacf5044961cca98798441eae101f889f
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2023-05-21 01:18:33 +0100

    Add implementation to header

--- morsecode/MorseRenderer.hpp
+++ morsecode/MorseRenderer.hpp
@@ -4,13 +4,37 @@
 #include <string>
 #include <map>
 #include <cstdint>
+#include <string>
 #include "pico/stdlib.h"
 
 template <uint16_t DitDuration>
 class MorseRenderer
 {
 public:
-    void Render(std::string inputString);
+    void Render(std::string inputString)
+    {
+        for (char inputChar : inputString)
+        {
+            for (char morseChar : morseMap.at(inputChar))
+            {
+                switch (morseChar)
+                {
+                case '.':
+                    Dit();
+                    break;
+                case '-':
+                    Dah();
+                    break;
+                case ' ':
+                    Space();
+                    break;
+                }
+                sleep_ms(signalSpaceDuration);
+            }
+
+            sleep_ms(charSpaceDuration);
+        }
+    }
 
 protected:
     static constexpr uint16_t ditDuration = DitDuration;
@@ -29,19 +53,73 @@ private:
         sleep_ms(wordSpaceDuration);
     }
 
-    static const std::map<char, const char *> morseMap;
+    static const std::map<char, std::string> morseMap;
 };
 
 template <uint16_t DitDuration>
-class LEDRenderer : MorseRenderer<DitDuration>
+class LEDRenderer : public MorseRenderer<DitDuration>
 {
 public:
-    LEDRenderer();
+    LEDRenderer()
+    {
+        gpio_init(ledPin);
+    }
 
 private:
-    void Dit();
-    void Dah();
+    void Dit()
+    {
+        gpio_put(ledPin, 1);
+        sleep_ms(MorseRenderer<DitDuration>::ditDuration);
+        gpio_put(ledPin, 0);
+    }
+
+    void Dah()
+    {
+        gpio_put(ledPin, 1);
+        sleep_ms(MorseRenderer<DitDuration>::dahDuration);
+        gpio_put(ledPin, 0);
+    }
     const uint ledPin = PICO_DEFAULT_LED_PIN;
 };
+
+template <uint16_t DitDuration>
+const std::map<char, std::string> MorseRenderer<DitDuration>::morseMap(
+    {{'A', ".-"},
+     {'B', "-..."},
+     {'C', "-.-."},
+     {'D', "-.."},
+     {'E', "."},
+     {'F', "..-."},
+     {'G', "--."},
+     {'H', "...."},
+     {'I', ".."},
+     {'J', ".---"},
+     {'K', "-.-"},
+     {'L', ".-.."},
+     {'M', "--"},
+     {'N', "-."},
+     {'O', "---"},
+     {'P', ".--."},
+     {'Q', "--.-"},
+     {'R', ".-."},
+     {'S', "..."},
+     {'T', "-"},
+     {'U', "..-"},
+     {'V', "...-"},
+     {'W', ".--"},
+     {'X', "-..-"},
+     {'Y', "-.--"},
+     {'Z', "--.."},
+     {'1', ".----"},
+     {'2', "..---"},
+     {'3', "...--"},
+     {'4', "....-"},
+     {'5', "....."},
+     {'6', "-...."},
+     {'7', "--..."},
+     {'8', "---.."},
+     {'9', "----."},
+     {'0', "-----"},
+     {' ', " "}});
 
 #endif
--- morsecode/main.cpp
+++ morsecode/main.cpp
@@ -1,19 +1,8 @@
 #include "pico/stdlib.h"
+#include "MorseRenderer.hpp"
 
 int main()
 {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true)
-    {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-    }
-#endif
+    MorseRenderer<500> *renderer = new LEDRenderer<500>();
+    renderer->Render("test");
 }
